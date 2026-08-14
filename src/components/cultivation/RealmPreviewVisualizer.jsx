93be6c79e614f9b1259b54e6e3116bec894e2e09
truevoidnan-commits <truevoidnan@gmail.com>
2026-08-14 20:59:39 +0700

    fix: xóa bỏ hoàn toàn 2 thanh cột trụ gỗ màu nâu 2 bên tháp

--- src/components/cultivation/RealmPreviewVisualizer.jsx
+++ src/components/cultivation/RealmPreviewVisualizer.jsx
@@ -412,10 +412,6 @@ export default function RealmPreviewVisualizer({ cultivation }) {
 
                     {/* Gian Điện Thần Tháp (Chamber Body) */}
                     <div className={styles.pagodaChamber}>
-                      {/* Đôi Cột Trụ Đúc Nổi 2 Bên */}
-                      <div className={styles.pillarLeft} />
-                      <div className={styles.pillarRight} />
-
                       {/* Nội Điện Chính Giữa (Center Void Realm) */}
                       <div className={styles.centerChamberVoid}>
                         {/* Biển Hiệu Tên Cung Treo Phía Trên */}
