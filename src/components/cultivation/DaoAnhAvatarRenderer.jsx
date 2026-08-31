14d6611551da5ca30a412bc7181249d27875dafd
truevoidnan-commits <truevoidnan@gmail.com>
2026-08-31 21:51:08 +0700

    fix: Khoi phuc toan bo thiet lap scale & toa do custom cho 42 Dao Anh trong Do Luc

--- src/components/cultivation/DaoAnhAvatarRenderer.jsx
+++ src/components/cultivation/DaoAnhAvatarRenderer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getDaoAnhEvolutionImage, getAssetUrl } from '../../lib/daoAnhData';
+import { getDaoAnhEvolutionImage, getDaoAnhTransformStyle, getAssetUrl } from '../../lib/daoAnhData';
 
 /**
  * DAO ANH AVATAR RENDERER
@@ -121,7 +121,7 @@ export default function DaoAnhAvatarRenderer({
               objectFit: 'contain',
               userSelect: 'none',
               imageRendering: '-webkit-optimize-contrast',
-              transform: 'none',
+              transform: getDaoAnhTransformStyle(daoAnh, currentKiep),
               transformOrigin: 'center center',
               backfaceVisibility: 'hidden',
               pointerEvents: 'none',
