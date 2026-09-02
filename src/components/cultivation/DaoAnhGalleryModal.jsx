5fc75e1079c64de1831b472145e97404a4bbab59
truevoidnan-commits <truevoidnan@gmail.com>
2026-09-02 09:32:40 +0700

    feat(dao_anh): Hoan thien 5 Kiep Hon Don Diet The Loi Tri va Thao Tu Kiem Quyet, hien thi Kiep 5 o Do Luc

--- src/components/cultivation/DaoAnhGalleryModal.jsx
+++ src/components/cultivation/DaoAnhGalleryModal.jsx
@@ -270,7 +270,7 @@ export default function DaoAnhGalleryModal({ isOpen, onClose }) {
                       <DaoAnhAvatarRenderer
                         daoAnh={da}
                         size={200}
-                        currentKiep={1}
+                        currentKiep={5}
                         animate={isHovered}
                         showAura={isHovered}
                       />
@@ -324,7 +324,7 @@ export default function DaoAnhGalleryModal({ isOpen, onClose }) {
                     <DaoAnhAvatarRenderer
                       daoAnh={da}
                       size={52}
-                      currentKiep={1}
+                      currentKiep={5}
                       animate={false}
                       showAura={false}
                     />
